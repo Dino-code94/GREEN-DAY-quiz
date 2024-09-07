0fbe6002c0f2be518fddb27f73d6eb63778f304a
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-09-07 22:52:03 +0000

    Adding second question

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,14 @@ const questions = [
             { text: "Wake Me Up When September Ends", correct: false},
             { text: "21st Century Breakdown", correct: false},   
         ]
+    },
+    {
+        question: "What other name does the song Time Of Your Life go by?",
+        answers: [
+            { text: "Tight Wad Hill", correct: false},
+            { text: "St. Jimmy", correct: false},
+            { text: "Good Riddance", correct: true},
+            { text: "Boulevard Of Broken Dreams", correct: false},
+        ] 
     }
    
