017169fd8e8db3e787d6c96e7a1efb6cbe9188a7
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-09-07 22:57:34 +0000

    Adding more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,5 +25,41 @@ const questions = [
             { text: "American Eulogy: Mass Hysteria", correct: false},
             { text: "I Believe In A Thing Called Love", correct: true},   
         ]
+    },
+    {
+        question: "The song Rusty James is found on what album?",
+        answers: [
+            { text: "Shenanigans", correct: false},
+            { text: "Nimrod", correct: false},
+            { text: "¡Uno!", correct: true},
+            { text: "Kerplunk", correct: false},   
+        ]
+    },
+    {
+        question: "Which one of these songs did Mike Dirnt solely write?",
+        answers: [
+            { text: "Emenius Sleepus", correct: true},
+            { text: "Graffitia", correct: false},
+            { text: "Junkies On A High", correct: false},
+            { text: "Wow! That's Loud", correct: false},   
+        ]
+    },
+    {
+        question: "Name this song: Get my television fix sitting on my crucifix?",
+        answers: [
+            { text: "Holiday", correct: false},
+            { text: "When I Come Around", correct: false},
+            { text: "Church on Sunday", correct: false},
+            { text: "Jesus Of Suburbia", correct: true},   
+        ]
+    },
+    {
+        question: "What are Green Day fans called?",
+        answers: [
+            { text: "The Idiot Club", correct: false},
+            { text: "Cast of American Idiot", correct: false},
+            { text: "Idiot Nation", correct: true},
+            { text: "Dickies", correct: false},   
+        ]
     }
    
