d32f42d92f0904f56da7fa1b64e0cd66ffffde95
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-09-07 22:50:19 +0000

    Adding first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const questions = [
     {
-        question: "What is the longest Green Day song?"
+        question: "What is the longest Green Day song?",
         answers: [
-            { text: "Homecoming", correct true}
-            { text: "Jesus of suburbia", correct false}
-            { text: "Forever now", correct false}
-            { text: "21st Century Breakdown", correct false}   
+            { text: "Homecoming", correct: true},
+            { text: "Sassafras Roots", correct: false},
+            { text: "Wake Me Up When September Ends", correct: false},
+            { text: "21st Century Breakdown", correct: false},   
         ]
-    },
-]
+    }
+   
