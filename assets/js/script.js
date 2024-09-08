0d6ba5e8476954f04c725ab698c4387f125db740
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2024-09-08 02:28:04 +0000

    Adding function correct, incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,91 +2,91 @@ const questions = [
     {
         question: "What is the longest Green Day song?",
         answers: [
-            { text: "Homecoming", correct: true},
-            { text: "Sassafras Roots", correct: false},
-            { text: "Wake Me Up When September Ends", correct: false},
-            { text: "21st Century Breakdown", correct: false},   
+            { text: "Homecoming", correct: true },
+            { text: "Sassafras Roots", correct: false },
+            { text: "Wake Me Up When September Ends", correct: false },
+            { text: "21st Century Breakdown", correct: false },
         ]
     },
     {
         question: "What other name does the song Time Of Your Life go by?",
         answers: [
-            { text: "Tight Wad Hill", correct: false},
-            { text: "St. Jimmy", correct: false},
-            { text: "Good Riddance", correct: true},
-            { text: "Boulevard Of Broken Dreams", correct: false},
-        ] 
+            { text: "Tight Wad Hill", correct: false },
+            { text: "St. Jimmy", correct: false },
+            { text: "Good Riddance", correct: true },
+            { text: "Boulevard Of Broken Dreams", correct: false },
+        ]
     },
     {
         question: "which of these 4 songs is not Green Day song?",
         answers: [
-            { text: "Letterbomb", correct: false},
-            { text: "A Little Boy Named Train", correct: false},
-            { text: "American Eulogy: Mass Hysteria", correct: false},
-            { text: "I Believe In A Thing Called Love", correct: true},   
+            { text: "Letterbomb", correct: false },
+            { text: "A Little Boy Named Train", correct: false },
+            { text: "American Eulogy: Mass Hysteria", correct: false },
+            { text: "I Believe In A Thing Called Love", correct: true },
         ]
     },
     {
         question: "The song Rusty James is found on what album?",
         answers: [
-            { text: "Shenanigans", correct: false},
-            { text: "Nimrod", correct: false},
-            { text: "¡Uno!", correct: true},
-            { text: "Kerplunk", correct: false},   
+            { text: "Shenanigans", correct: false },
+            { text: "Nimrod", correct: false },
+            { text: "¡Uno!", correct: true },
+            { text: "Kerplunk", correct: false },
         ]
     },
     {
         question: "Which one of these songs did Mike Dirnt solely write?",
         answers: [
-            { text: "Emenius Sleepus", correct: true},
-            { text: "Graffitia", correct: false},
-            { text: "Junkies On A High", correct: false},
-            { text: "Wow! That's Loud", correct: false},   
+            { text: "Emenius Sleepus", correct: true },
+            { text: "Graffitia", correct: false },
+            { text: "Junkies On A High", correct: false },
+            { text: "Wow! That's Loud", correct: false },
         ]
     },
     {
         question: "Name this song: Get my television fix sitting on my crucifix?",
         answers: [
-            { text: "Holiday", correct: false},
-            { text: "When I Come Around", correct: false},
-            { text: "Church on Sunday", correct: false},
-            { text: "Jesus Of Suburbia", correct: true},   
+            { text: "Holiday", correct: false },
+            { text: "When I Come Around", correct: false },
+            { text: "Church on Sunday", correct: false },
+            { text: "Jesus Of Suburbia", correct: true },
         ]
     },
     {
         question: "What are Green Day fans called?",
         answers: [
-            { text: "The Idiot Club", correct: false},
-            { text: "Cast of American Idiot", correct: false},
-            { text: "Idiot Nation", correct: true},
-            { text: "Dickies", correct: false},   
+            { text: "The Idiot Club", correct: false },
+            { text: "Cast of American Idiot", correct: false },
+            { text: "Idiot Nation", correct: true },
+            { text: "Dickies", correct: false },
         ]
     },
     {
         question: "Who was Green Day's original drummer?",
         answers: [
-            { text: "Tre Cool", correct: false},
-            { text: "Jason Freese", correct: false},
-            { text: "Jason White", correct: false},
-            { text: "John Kiffmeyer", correct: true},   
-        ]  
+            { text: "Tre Cool", correct: false },
+            { text: "Jason Freese", correct: false },
+            { text: "Jason White", correct: false },
+            { text: "John Kiffmeyer", correct: true },
+        ]
     },
     {
         question: "What is Green Day's first hit?",
         answers: [
-            { text: "Who Wrote Holden Caulfield", correct: false},
-            { text: "Longview", correct: true},
-            { text: "Basket Case", correct: false},
-            { text: "21 Guns", correct: false},
-        ]  
+            { text: "Who Wrote Holden Caulfield", correct: false },
+            { text: "Longview", correct: true },
+            { text: "Basket Case", correct: false },
+            { text: "21 Guns", correct: false },
+        ]
     },
     {
         question: "Which album was Green Day's first?",
         answers: [
-            { text: "1,039/Smoothed Out Slappy Hours", correct: true},
-            { text: "Saviours", correct: false},
-            { text: "Warning", correct: false},
-            { text: "Cigarettes and Valentines", correct: false},   
+            { text: "1,039/Smoothed Out Slappy Hours", correct: true },
+            { text: "Saviours", correct: false },
+            { text: "Warning", correct: false },
+            { text: "Cigarettes and Valentines", correct: false },
         ]
     }
 ];
@@ -98,37 +98,47 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex +1;
+    let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
+        question;
 
-    
+
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answer.correct){
+        if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
     });
 }
 
-function resetState(){
+function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild){
+    while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct")
+    } else {
+        selectedBtn.classList.add("incorrect");
     }
 }
 
@@ -140,4 +150,3 @@ startQuiz();
 
 
 
-   
